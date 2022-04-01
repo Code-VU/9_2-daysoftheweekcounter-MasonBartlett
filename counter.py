2625e57a2bf027ed30fe98d8adadd37d740b449c
Mason Bartlett <Mason.Bartlett@vu.com>
2022-03-31 20:59:06 -0500

    First try

--- counter.py
+++ counter.py
@@ -1,10 +1,27 @@
 
+
 def countDayOfTheWeek():
     # This first line is provided for you
+    dayCount = dict()
     file_name = input("Enter a file name: ")    # end assignment
+
+    fileInfo = open(file_name)
+
+    for line in fileInfo:
+        if line.startswith('From '):
+            positions = line.split()
+            #print(positions)
+            dayData = (positions[2])
+            dayCount[dayData]=dayCount.get(dayData, 0)+1
+        
+        else:
+            continue
+    print(dayCount)
+
+#      mbox-short.txt    
 
 ## if you want to test locally before you try to sync
 ## uncomment calculateAbsolute() and run > python payCalculator.py
 ## ***IMPORTANT*** please recomment before you submit/sync your assignment.
 ## OR YOUR TEST WILL NOT RUN
-##countDayOfTheWeek()
+#countDayOfTheWeek()
